c657d6648059f6c145fbd2ca1d0fb04725462ccc
James Rozsypal <jimmerxl@gmail.com>
2022-03-15 15:38:50 -0400

    Re-styled the add book button and added login button (keeping login hidden for now)

--- app.js
+++ app.js
@@ -1,19 +1,31 @@
 let myLibrary = [];
 
-function Book(title, author, pages, read) {
+function Book(title, author, pages, isRead) {
   this.title = title;
   this.author = author;
   this.pages = pages;
-  this.read = read;
+  this.isRead = isRead;
 
   this.info = () => {
     return console.log(
-      this.title + ", " + this.author + ", " + this.pages + ", " + this.read
+      this.title + ", " + this.author + ", " + this.pages + ", " + this.isRead
     );
   };
 }
 
-function addBookToLibrary() {
+function addBookToLibrary(title, author, pages, isRead) {
+  
+}
+
+function getBookCard(book) {
+  const card = document.createElement('div');
+  const title = document.createElement('p');
+  const author = document.createElement('p');
+  const pages = document.createElement('p');
+  const isRead = document.createElement('p');
+  const deleteBtn = document.createElement('button');
+
+  card.classList.add('card');
   
 }
 
