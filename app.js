ec246d3e734e5ce24656e3fa5a198b42c6ace375
James Rozsypal <jimmerxl@gmail.com>
2022-03-21 16:21:36 -0400

    Add cards from the myLibrary array (js) to the library-grid (html)

--- app.js
+++ app.js
@@ -1,33 +1,117 @@
+const libraryGrid = document.querySelector(".library");
+const btnAddBook = document.querySelector(".btn-add-book");
+const btnDeleteBook = document.querySelector(".btn-delete-book");
+
 let myLibrary = [];
 
-function Book(title, author, pages, isRead) {
+function Book(title, author, genre, pages, isRead) {
   this.title = title;
   this.author = author;
+  this.genre = genre;
   this.pages = pages;
   this.isRead = isRead;
 
   this.info = () => {
     return console.log(
-      this.title + ", " + this.author + ", " + this.pages + ", " + this.isRead
+      this.title +
+        ", " +
+        this.author +
+        ", " +
+        this.genre +
+        ", " +
+        this.pages +
+        ", " +
+        this.isRead
     );
   };
 }
 
-function addBookToLibrary(title, author, pages, isRead) {
+function addBooksToLibrary(library = []) {
+  for (i in library) {
+    let currBook = library[i];
+    let currCard = bookCard(currBook);
+  }
+}
+
+function bookCard(book) {
+  const card = document.createElement("div");
+  const title = document.createElement("h3");
+  const author = document.createElement("p");
+  const genre = document.createElement("p");
+  const pages = document.createElement("p");
+  const read = document.createElement("div");
+  const isRead = document.createElement("p");
+  const ifRead = document.createElement("input");
+  const deleteBtn = document.createElement("button");
+
+  card.classList.add("book-card");
+  title.classList.add("title");
+  author.classList.add("author");
+  genre.classList.add("genre");
+  pages.classList.add("pages");
+  read.classList.add("read");
+  isRead.classList.add("is-read");
+  ifRead.setAttribute("type", "checkbox");
+  ifRead.classList.add("if-read");
+  deleteBtn.classList.add("btn-delete-book");
+
+  title.textContent = book.title;
+  author.textContent = book.author;
+  genre.textContent = book.genre;
+  pages.textContent = book.pages + " pages";
+  isRead.textContent = book.isRead;
+  deleteBtn.textContent = "Delete";
+
+  if (isRead.textContent === "Read") {
+    isRead.style.color = "var(--is-read-color)";
+  } else {
+    isRead.style.color = "var(--delete-bg-color)";
+  }
+
+  ifRead.addEventListener("change", () => {
+    if (ifRead.checked === true) {
+      isRead.textContent = "Read";
+      isRead.style.color = "var(--is-read-color)";
+    } else {
+      isRead.textContent = "Not Read";
+      isRead.style.color = "var(--delete-bg-color)";
+    }
+  });
+
+  // deleteBtn.addEventListener("")
+
+  card.appendChild(title);
+  card.appendChild(author);
+  card.appendChild(genre);
+  card.appendChild(pages);
+  read.appendChild(isRead);
+  read.appendChild(ifRead);
+  card.appendChild(read);
+  card.appendChild(deleteBtn);
+  libraryGrid.appendChild(card);
+}
+
+function checkIfRead(e) {
   
 }
 
-function getBookCard(book) {
-  const card = document.createElement('div');
-  const title = document.createElement('p');
-  const author = document.createElement('p');
-  const pages = document.createElement('p');
-  const isRead = document.createElement('p');
-  const deleteBtn = document.createElement('button');
+const theHobbit = new Book(
+  "The Hobbit",
+  "J.R.R. Tolkien",
+  "Fantasy Fiction",
+  "295",
+  "Not read"
+);
 
-  card.classList.add('card');
-  
-}
+const chamberOfSecrets = new Book(
+  "Harry Potter 2",
+  "J.K. Rowling",
+  "Fantasy Fiction",
+  "341",
+  "Not read"
+);
 
-const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", "295", "Not read");
-theHobbit.info();
+myLibrary.push(theHobbit);
+myLibrary.push(chamberOfSecrets);
+
+addBooksToLibrary(myLibrary);
